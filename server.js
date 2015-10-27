149bb1e6cf948db9ccc3cf0e96935d3cac58071f
Justin Mancherje <mancherje.justin@gmail.com>
2015-10-27 10:28:03 -0700

    Add ?completed filter

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ app.get('/', function (req, res) {
 
 // GET request /todos (to get all todos)
 app.get('/todos', function (req, res) {
-  res.json(todos);
+  var queryParams = req.query;
+  var filteredTodos = todos;
+
+  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+    filteredTodos = _.where(filteredTodos, {completed: true});
+  } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+    filteredTodos = _.where(filteredTodos, {completed: false});
+  }
+
+  res.json(filteredTodos);
 });
 // GET /todos/:id (to get todo id:1 etc)
 app.get('/todos/:id', function (req, res) {
