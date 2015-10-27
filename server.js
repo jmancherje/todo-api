069e1b4b0281c28ed06cf1e597c2e255129a9143
Justin Mancherje <mancherje.justin@gmail.com>
2015-10-26 22:04:58 -0700

    Add DELETE todos route

--- server.js
+++ server.js
@@ -51,6 +51,27 @@ app.post('/todos', function (req, res) {
   res.json(body);
 });
 
+
+// DELETE /todos/:id
+app.delete('/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id: todoId});
+
+  if (!matchedTodo) {
+    res.status(404).json({"error": "no todo found with that id"});
+  } else {
+    todos = _.without(todos, matchedTodo);
+    // essentially returning matched Todo
+    res.json(matchedTodo);
+  }
+  
+  res.status(200).send();
+
+});
+
+
+
+
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
 });
